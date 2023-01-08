7e8b6a634e9d0ca52b4e62e638e3c03d4b55e69b
Om Dhawan <106106159+Om-Dhawan@users.noreply.github.com>
2023-01-08 06:45:57 +0530

    Add files via upload

---
+++ realtime_detection.py
@@ -0,0 +1,51 @@
+import cv2
+
+# Opencv DNN
+net = cv2.dnn.readNet("./object_detection/dnn_model/yolov4-tiny.weights", "./object_detection/dnn_model/yolov4-tiny.cfg")
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(320, 320), scale=1/255)
+
+# Load class lists
+classes = []
+with open("./object_detection/dnn_model/classes.txt", "r") as file_object:
+    for class_name in file_object.readlines():
+        class_name = class_name.strip()
+        classes.append(class_name)
+
+# print("Objects list")
+# print(classes)
+
+
+# Initialize camera
+cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(3,1920)
+cap.set(4,1080)
+# FULL HD 1920 x 1080
+
+
+# Create window
+cv2.namedWindow("Frame")
+
+while cap.isOpened():
+    # Get frames
+    ret, frame = cap.read()
+
+    # Object Detection
+    (class_ids, scores, bboxes) = model.detect(frame, confThreshold=0.3, nmsThreshold=.4)
+    for class_id, score, bbox in zip(class_ids, scores, bboxes):
+        (x, y, w, h) = bbox
+        class_name = classes[class_id]
+        class_name=class_name+" "+str(round(score*100,3))+"%"
+
+        cv2.putText(frame, class_name, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (73, 200, 173), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (173, 200, 73), 3)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
